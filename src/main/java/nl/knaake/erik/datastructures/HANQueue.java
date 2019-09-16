b107edcd71b0d698c6cdce4be5b7afcae8d23e09
Erik Knaake <e.knaake@gmail.com>
2019-09-16 14:13:01 +0200

    added queue

---
+++ src/main/java/nl/knaake/erik/datastructures/HANQueue.java
@@ -0,0 +1,70 @@
+package nl.knaake.erik.datastructures;
+
+public class HANQueue<T> {
+    private T[] queue;
+    private int size;
+    private int front;
+    private int back;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public HANQueue() {
+        queue = makeNewArray(DEFAULT_INITIAL_CAPACITY);
+        makeEmpty();
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void makeEmpty() {
+        front = 0;
+        back = -1;
+        size = 0;
+    }
+
+    public T dequeue() {
+        if(isEmpty())
+            return null;
+        size--;
+        T result = queue[front];
+        front = increment(front);
+        return result;
+    }
+
+    public T getFront() {
+        if(isEmpty())
+            return null;
+        return queue[front];
+    }
+
+    public void enqueue(T item) {
+        if(size == queue.length)
+            doubleQueue();
+        back = increment(back);
+        queue[back] = item;
+        size++;
+    }
+
+    private T[] makeNewArray(int size) {
+        return (T[])new Object[size];
+    }
+
+    private void doubleQueue() {
+        T[] newQueue = makeNewArray(queue.length * 2);
+        for(int i = 0; i < size; i++, front = increment(front))
+            newQueue[i] = queue[front];
+        queue = newQueue;
+        front = 0;
+        back = size - 1;
+    }
+
+    private int increment(int x) {
+        if(++x == queue.length)
+            x = 0;
+        return x;
+    }
+}
---
+++ src/test/java/nl/knaake/erik/datastructures/HANQueueTest.java
@@ -0,0 +1,90 @@
+package nl.knaake.erik.datastructures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class HANQueueTest {
+    private HANQueue<String> queue;
+
+    @Before
+    public void before() {
+        queue = new HANQueue<>();
+    }
+
+    @Test
+    public void enqueueTest() {
+        queue.enqueue("Hello");
+        assertEquals("Hello", queue.getFront());
+    }
+
+    @Test
+    public void dequeueTest() {
+        queue.enqueue("Hello");
+        assertEquals("Hello", queue.dequeue());
+    }
+
+    @Test
+    public void dequeueShouldReturnNullOnEmptyQueue() {
+        assertNull(queue.dequeue());
+    }
+
+    @Test
+    public void getFrontShouldReturnNullOnEmptyQueue() {
+        assertNull(queue.getFront());
+    }
+
+    @Test
+    public void emptySize() {
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    public void size() {
+        queue.enqueue("Hello");
+        assertEquals(1, queue.size());
+    }
+
+    @Test
+    public void sizeAfterDequeue() {
+        queue.enqueue("Hello");
+        queue.dequeue();
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    public void enqueueLargerThenArrayTest() {
+        for(int i = 0; i < 18; i++)
+            queue.enqueue("Hello" + i);
+        assertEquals(18, queue.size());
+    }
+
+    @Test
+    public void makeEmptyShouldResetSize() {
+        queue.enqueue("hello");
+        queue.makeEmpty();
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    public void makeEmptyShouldPreventReadsToPreAllocated() {
+        queue.enqueue("hello");
+        queue.makeEmpty();
+        assertNull(queue.getFront());
+    }
+
+    @Test
+    public void overflowAfterDequeue() {
+        for(int i = 0; i < 15; i++)
+            queue.enqueue("Hello" + i);
+        queue.dequeue();
+        queue.enqueue("HelloEn1");
+        queue.enqueue("HelloEn2");
+        for(int i = 1; i < 15; i++)
+            assertEquals("Hello" + i, queue.dequeue());
+        assertEquals("HelloEn1", queue.dequeue());
+        assertEquals("HelloEn2", queue.dequeue());
+    }
+}
