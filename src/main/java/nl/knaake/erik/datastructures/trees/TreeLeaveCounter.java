2e850c60951dec7783bbaa2edee9cbe27f45ed81
Erik Knaake <e.knaake@gmail.com>
2019-09-19 22:55:12 +0200

    added not null annotations where missing

--- src/main/java/nl/knaake/erik/datastructures/trees/TreeLeaveCounter.java
+++ src/main/java/nl/knaake/erik/datastructures/trees/TreeLeaveCounter.java
@@ -1,14 +1,16 @@
 package nl.knaake.erik.datastructures.trees;
 
+import org.jetbrains.annotations.NotNull;
+
 public class TreeLeaveCounter<T> implements IApplyTree<T, Integer> {
 
     @Override
-    public Integer apply(Tree<T> tree) {
+    public Integer apply(@NotNull Tree<T> tree) {
         return apply(tree.root);
     }
 
     @Override
-    public Integer apply(TreeNode<T> node) {
+    public Integer apply(@NotNull TreeNode<T> node) {
         int currentNumber = 0;
         if(node.firstChild != null)
             currentNumber += apply(node.firstChild);
